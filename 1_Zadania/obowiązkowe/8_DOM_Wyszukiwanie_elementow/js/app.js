182755b9d7ae35611a4b2cee0e46fbc2f0201880
Ryczek Szymon <szymon.ryczek@microsoft.wsei.edu.pl>
2019-12-08 09:15:01 +0100

    Add obowiazkowe/8

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -1,6 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     /*
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
-     */
+     */ 
 
+    // zadanie 0
+    const title = document.querySelector('.title');
+    function getDataAnimation(element) {
+        return element.getAttribute('data-animation');
+    }
+    getDataAnimation(title);
+
+    // zadanie 1
+    const home1 = document.querySelector('#home');
+    const home2 = document.getElementById('home');
+
+    const firstDataDirection = document.querySelector('[data-direction]');
+    const firstBlockElement = document.querySelector('.block');
+    
+    // zadanie 2
+    const liElements = document.querySelectorAll('nav li');
+    const pElements = document.querySelectorAll('div p');
+    const divElements = document.querySelectorAll('article div');
+
+    // zadanie 3
+    const articleFirst = document.querySelector('.first');
+
+    if (articleFirst) {
+        const h1 = articleFirst.querySelectorAll('h1');
+        console.log('zadanie 3', h1.length)
+    }
 });
