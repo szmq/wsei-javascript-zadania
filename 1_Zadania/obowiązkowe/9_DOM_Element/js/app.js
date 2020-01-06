62a85c033fb23edfd381d12933d0e9e78541f319
Szymon Ryczek <szymon.ryczek@sinch.com>
2020-01-06 23:26:37 +0100

    Add 9_DOM_Element

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    // Zadanie 0
+
     function getDatasInfo(elements) {
         const nElements = [];
 
@@ -19,5 +21,87 @@ document.addEventListener("DOMContentLoaded", function () {
         return nElements;
     }
 
-    getDatasInfo(links);
+    getDatasInfo(links); // ["blue", "red", "green", "pink"]
+
+    // Zadanie 1
+
+    function writeElements(element) {
+        if (element.length === undefined) {
+            console.log(element);
+            console.log("Class name: " + element.className);
+            console.log("Tag name: " + element.tagName);
+        } else {
+            [...element].forEach((el) => {
+                console.log(el);
+                console.log("Class name: " + el.className);
+                console.log("Tag name: " + el.tagName);
+            });
+        }
+    }
+
+    writeElements(homeElement);
+    writeElements(childElements);
+    writeElements(banner);
+    writeElements(blocks);
+    writeElements(links);
+
+    // Zadanie 2
+
+    for (let i = 0; i < blocks.length; i++) {
+        console.log(blocks[i].innerHTML); // Zwraca html potomków elementu
+        console.log(blocks[i].outerHTML); // Zwraca html elementu i jego potomków
+        blocks[i].innerHTML = "Nowa wartość diva o klasie blocks";
+        console.log(blocks[i].innerHTML); // Hiperłącza zostały zastąpione plain textem
+    }
+
+    // Zadanie 3
+    const mainFooter = document.querySelector("#mainFooter");
+    function getId(element) {
+        return element.id;
+    }
+
+    getId(mainFooter);
+
+    // Zadanie 4
+    function getTags(elements) {
+        const foo = [];
+
+        [...elements].forEach((item) => {
+            foo.push(item.tagName);
+        });
+
+        return foo;
+    }
+    getTags(childElements);
+
+    // Zadanie 5
+    function getClassInfo(elements){
+        const foo = [];
+
+        if (elements.length === undefined) {
+            foo.push(elements.className);
+            return foo;
+        }
+
+        [...elements].forEach((item) => {
+            foo.push(item.className);
+        });
+
+        return foo;
+    }
+
+    getClassInfo(banner);
+
+    // Zadanie 6
+    const li = document.querySelectorAll("nav li");
+
+    function setDataDirection(elements) {
+        for(let i = 0; i < elements.length; i++){
+            if(elements[i].dataset.direction === undefined){
+                elements[i].dataset.direction = "top";
+            }
+        }
+    }
+
+    setDataDirection(li);
 });
