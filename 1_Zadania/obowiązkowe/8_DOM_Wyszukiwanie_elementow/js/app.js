e9362ab1580c904516892add8c79e603e6dedea7
Szymon Ryczek <szymon.ryczek@sinch.com>
2020-01-06 22:45:54 +0100

    Add 8_DOM_Wyszukiwanie_elementow

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -3,26 +3,30 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */ 
 
-    // zadanie 0
+    // Zadanie 0
+
     const title = document.querySelector('.title');
     function getDataAnimation(element) {
         return element.getAttribute('data-animation');
     }
     getDataAnimation(title);
 
-    // zadanie 1
+    // Zadanie 1
+
     const home1 = document.querySelector('#home');
     const home2 = document.getElementById('home');
 
     const firstDataDirection = document.querySelector('[data-direction]');
     const firstBlockElement = document.querySelector('.block');
     
-    // zadanie 2
+    // Zadanie 2
+
     const liElements = document.querySelectorAll('nav li');
     const pElements = document.querySelectorAll('div p');
     const divElements = document.querySelectorAll('article div');
 
-    // zadanie 3
+    // Zadanie 3
+
     const articleFirst = document.querySelector('.first');
 
     if (articleFirst) {
